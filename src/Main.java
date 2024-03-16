f9cbc9553682ad22b8380b513d96252f1924ff59
Med Amine Slama <mohammedaminslama@gmail.com>
2024-03-16 21:14:46 +0100

    Add more spaghet comments

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
 import java.util.*;
 import java.applet.*;
 
+
+// This is a comment in Java yay!
+/*
+ * Time to write more comments
+ * This is the main branch that will handle all the functionality provided bu other
+ * Packages, packages will vary depending on the amount of effort im willing to give
+ * into java shenanigins.
+ */
+
 // This is to test the capability of my understanding of git.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
-
