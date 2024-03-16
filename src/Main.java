a3b917e66d66d41819a6434cc95e291894d9322f
SlamZDank <83764499+SlamZDank@users.noreply.github.com>
2024-03-16 20:55:39 +0100

    init
    dumb project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+// This is to test the capability of my understanding of git.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
