11e4d0085c4d393b3b27e0a34b423f87af138504
Med Amine Slama <mohammedaminslama@gmail.com>
2024-03-17 18:18:44 +0100

    Enumify notes

--- src/elements/Note.java
+++ src/elements/Note.java
@@ -1,19 +1,49 @@
-
 package elements;
+import java.util.Scanner;
 
-public class Note {
+
+public enum Note {
+    /*
+     * Implemets method that requires you to type each note and each coefficient
+     * You could change that so the coefficient become hardtyped
+     * 
+     * Added additional getters and setters to see what you need
+     */
+
+// Example of implementation with hard coded coef value:
+    // Math(1.0),
+    //       ^  Coefficient
+    Math(), 
+    Physique(), 
+    Litteraire(), 
+    Chimie(), 
+    SVT(), 
+    Histoire(), 
+    Geographie(), 
+    Francais(), 
+    Anglais(), 
+    Allmagne();
+
     private String Mat;
     private double Note;
     private double Coef;
 
-    public Note(String Mat, double Note, double Coef) {
-        this.Mat = Mat;
-        this.Note = Note;
+    private Note(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Matière: " + this.name() + ": ");
+        System.out.println("Donner la note: ");
+        Note = sc.nextDouble();
+        System.out.println("Enter le Coefficient: ");
+        Coef = sc.nextDouble();
+        sc.close();
+    }
+
+    private Note(double Coef) {
         this.Coef = Coef;
     }
 
     public double getCoef(){return Coef;}
     public double getNote() {return Note;}
     public void setCoef(double coef) {Coef = coef;}
-    public void setMat(String mat) {Mat = mat;}
+    public void setNote(double note) {Note = note;}
 }
