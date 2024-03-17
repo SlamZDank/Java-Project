d0676c75045a33354a36b88d4c587327fd032451
Med Amine Slama <mohammedaminslama@gmail.com>
2024-03-17 16:55:22 +0100

    notes

---
+++ src/elements/Note.java
@@ -0,0 +1,18 @@
+package elements;
+
+public class Note {
+    private String Mat;
+    private double Note;
+    private double Coef;
+
+    public Note(String Mat, double Note, double Coef) {
+        this.Mat = Mat;
+        this.Note = Note;
+        this.Coef = Coef;
+    }
+
+    
+}
+
+
+
