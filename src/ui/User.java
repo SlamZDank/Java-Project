64fb2db91b3def54cadacfffd232ed0a00788b78
Med Amine Slama <mohammedaminslama@gmail.com>
2024-03-19 13:57:40 +0100

    s

--- src/ui/User.java
+++ src/ui/User.java
@@ -4,7 +4,7 @@ package ui;
 /*
  * Implement Frameworks that makes it easy to create UI elements
  * 
- * Plain forum where to include data such as your name, age, notes.
+ * Plain form where to include data such as your name, age, notes.
  * The score will be calculated automatically
  */
 public class User {
